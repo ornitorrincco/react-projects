0d60cb3171c7d976b84ce194adbc6c8a4ca12f3d
ornitorrincco <ornitorrincco@gmail.com>
2017-12-28 10:05:35 -0800

    initial state in AlbumList added

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
+class AlbumList extends Component {
+
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
