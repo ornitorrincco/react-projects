59e818fd9693c125185584b912bcb714e7374219
ornitorrincco <ornitorrincco@gmail.com>
2017-12-30 20:18:19 -0800

    linking to amazon buy button and text button pass by children prop added

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
   return (
-    <TouchableOpacity style={buttonStyle}>
-      <Text style={textStyle}>Click me</Text>
+    //father pass to this children from AlbumDetails of button onPress(callback)
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>
+        {children}
+      </Text>
     </TouchableOpacity>
   );
 };
