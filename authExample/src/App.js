47d344b1ff30ae62c92df2ff0e7df3f795da8dde
ornitorrincco <ornitorrincco@gmail.com>
2018-01-01 18:59:18 -0800

    logout UI added and refactor of change state for sign/logout process

--- authExample/src/App.js
+++ authExample/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAgeKM5mHLtIpglsiOSXXwszFjh8RCiAA4',
@@ -14,13 +16,37 @@ class App extends Component {
       storageBucket: 'authentication-b8a07.appspot.com',
       messagingSenderId: '204603754319'
   });
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
 }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+            Log out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
+  }
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+          {this.renderContent()}
       </View>
     );
   }
